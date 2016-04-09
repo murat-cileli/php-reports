5d0882971af0b9044107fb9f302d18c2978260a9
Persona Non Grata <Persona Non Grata>
2016-04-09 22:24:16 +0300

    Update

--- src/PHPReportz.php
+++ src/PHPReportz.php
@@ -8,9 +8,23 @@ namespace PHPReportz;
  */
 class PHPReportz implements \JsonSerializable
 {
+
+    /**
+     * Output file type selection
+     */
+    const OUTPUT_DOCX = 1;
+    const OUTPUT_PDF = 2;
+
+    /**
+     * Get download URL as JSON or direct download report
+     */
+    const ACTION_GET_DOWNLOAD_URL = 3;
+    const ACTION_FORCE_DOWNLOAD = 4;
+
     protected $api_key;
     protected $template_id;
     protected $parameters;
+    private $json;
 
     /**
      * PHPReportz constructor.
@@ -18,7 +32,7 @@ class PHPReportz implements \JsonSerializable
      */
     public function __construct($api_key)
     {
-        if (is_null($api_key) || strlen($api_key) != 24) {
+        if (empty($api_key) || strlen($api_key) != 24) {
             throw new \Exception("Invalid API key");
         }
 
@@ -26,11 +40,23 @@ class PHPReportz implements \JsonSerializable
     }
 
     /**
+     * @return mixed
+     */
+    private function getApiKey()
+    {
+        if (empty($api_key)) {
+            throw new \Exception("Invalid API key");
+        }
+
+        return $this->api_key;
+    }
+
+    /**
      * @param mixed $template_id
      */
     public function setTemplateId($template_id)
     {
-        if (is_null($template_id) || !is_int($template_id)) {
+        if (empty($template_id) || !is_int($template_id)) {
             throw new \Exception("Invalid Template ID");
         }
 
@@ -38,11 +64,23 @@ class PHPReportz implements \JsonSerializable
     }
 
     /**
+     * @return mixed
+     */
+    private function getTemplateİd()
+    {
+        if (empty($template_id)) {
+            throw new \Exception("Invalid Template ID");
+        }
+
+        return $this->template_id;
+    }
+
+    /**
      * @param mixed $parameters
      */
     public function setParameters($parameters)
     {
-        if (is_null($parameters) || !is_array($parameters)) {
+        if (empty($parameters) || !is_array($parameters)) {
             throw new \Exception("Parameters must be an array");
         }
 
@@ -50,14 +88,26 @@ class PHPReportz implements \JsonSerializable
     }
 
     /**
+     * @return mixed
+     */
+    private function getParameters()
+    {
+        if (empty($parameters)) {
+            throw new \Exception("Parameters must be an array");
+        }
+
+        return $this->parameters;
+    }
+
+    /**
      * @return array
      */
     public function jsonSerialize()
     {
-        return [
-            'api_key'     => $this->api_key,
-            'template_id' => $this->template_id,
-            'parameters'  => $this->parameters
+        $this->json = [
+            'api_key'     => $this->getApiKey(),
+            'template_id' => $this->getTemplateİd(),
+            'parameters'  => $this->getParameters()
         ];
     }
 }
