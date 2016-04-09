c69bda8047643fbfc455fd7fa9ba5d799f8cf136
Persona Non Grata <Persona Non Grata>
2016-04-09 21:18:18 +0300

    Update

--- src/PHPReportz.php
+++ src/PHPReportz.php
@@ -2,16 +2,62 @@
 
 namespace PHPReportz;
 
-class PHPReportz
+/**
+ * Class PHPReportz
+ * @package PHPReportz
+ */
+class PHPReportz implements \JsonSerializable
 {
     protected $api_key;
     protected $template_id;
     protected $parameters;
 
+    /**
+     * PHPReportz constructor.
+     * @param $api_key
+     */
     public function __construct($api_key)
     {
-        if (is_null($api_key) || strlen($api_key) != 20) {
+        if (is_null($api_key) || strlen($api_key) != 24) {
             throw new \Exception("Invalid API key");
         }
+
+        $this->api_key = $api_key;
+    }
+
+    /**
+     * @param mixed $template_id
+     */
+    public function setTemplateId($template_id)
+    {
+        if (is_null($template_id) || !is_int($template_id)) {
+            throw new \Exception("Invalid Template ID");
+        }
+
+        $this->template_id = $template_id;
+    }
+
+    /**
+     * @param mixed $parameters
+     */
+    public function setParameters($parameters)
+    {
+        if (is_null($parameters) || !is_array($parameters)) {
+            throw new \Exception("Parameters must be an array");
+        }
+
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'api_key'     => $this->api_key,
+            'template_id' => $this->template_id,
+            'parameters'  => $this->parameters
+        ];
     }
 }
--- tests/index.php
+++ tests/index.php
@@ -1,6 +1,18 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+//require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once '../src/PHPReportz.php';
 
 use PHPReportz\PHPReportz;
 
-$pr = new PHPReportz('a');
+$pr = new PHPReportz('123456789012345678901234');
+$pr->setTemplateId(1);
+$pr->setParameters(
+    array(
+        'company_name' => 'Apple Inc.',
+        'address' => 'Istanbul / Turkey',
+        'client_name' => 'Murat Çileli',
+        'total' => '0.55'
+    ));
+
+echo json_encode($pr);
